0abd3a446e7bdfd052b76cfc029d5bb38e96ad9e
Raoul <sulimoviciraoul@yahoo.com>
2022-04-15 01:09:39 +0300

    fixed center for navbar links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,30 +5,32 @@ import { UserCircleIcon } from "@heroicons/react/outline";
 
 export const Navbar = () => {
   return (
-    <nav className="px-2 sm:px-4 py-2.5">
-      <div className="flex justify-between items-center mx-auto">
-        <Link to="/" className="flex items-center">
-          <Logo class="mr-2 h-12 sm:h-12" alt="WarScape Logo" />
-          <div className="text-3xl font-bold colorGreen mt-1.5">WarScape</div>
-        </Link>
+    <nav className="flex justify-between items-center mx-auto px-2 sm:px-4 py-2.5">
+      <Link to="/" className="flex items-center basis-1/3">
+        <Logo className="mr-2 h-12 sm:h-12" alt="WarScape Logo" />
+        <div className="text-3xl font-bold colorGreen mt-1.5">WarScape</div>
+      </Link>
 
-        <div>a</div>
+      <div className="flex basis-1/3">
+        <div className="mr-2">a</div>
+        <div className="mr-2">b</div>
+        <div className="mr-2">c</div>
+      </div>
 
-        <div>
-          {true /* is_logged_in */ ? (
-            <UserCircleIcon
-              onClick={() => {}}
-              className="h-10 w-10 colorRed cursor-pointer"
-            />
-          ) : (
-            <Button
-              onClick={() => {}}
-              btnColor="btnRed"
-              text="Sign in"
-              size="md"
-            />
-          )}
-        </div>
+      <div>
+        {true /* is_logged_in */ ? (
+          <UserCircleIcon
+            onClick={() => {}}
+            className="h-10 w-10 colorRed cursor-pointer"
+          />
+        ) : (
+          <Button
+            onClick={() => {}}
+            btnColor="btnRed"
+            text="Sign in"
+            size="md"
+          />
+        )}
       </div>
     </nav>
   );
