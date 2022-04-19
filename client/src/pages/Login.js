a77a929f0778bef818f71721483e1937ebae3234
Alexandru Popa <alex9popa@yahoo.com>
2022-04-19 19:29:11 +0300

    50% login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,21 @@
 export const Login = () => {
-  return "";
+  return (
+    <div className="login">
+      <div className="form">
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+        </div>
+        <div className="button-container">
+          <form action="/home" method="get">
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    </div>
+  )
 };
