2abc3b1df4286fb67fc0834922bdd1d64d74866a
Raoul <sulimoviciraoul@yahoo.com>
2022-05-11 22:44:03 +0300

    scroll fix

--- client/src/components/card/modals/info-modal/FooterItem.js
+++ client/src/components/card/modals/info-modal/FooterItem.js
@@ -1,31 +1,38 @@
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
+
 export const FooterItem = ({ content, title }) => {
   return (
     <div className="footerItemContainer">
       <div className="footerItemTitle">{title}</div>
-      <div className="footerItemBody">
-        {content.map((c, index) => {
-          return (
-            <div
-              className={`footerItemRow ${
-                index === content.length - 1 ? "border-b-2" : ""
-              }`}
-              key={`resource-${index}`}
-            >
-              {title === "Resources" ? (
-                <div className="flex flex-col">
-                  <span>Type: {c.type}</span>
-                  <span>Quantity: {c.quantity}</span>
-                </div>
-              ) : (
-                <div className="flex flex-col">
-                  <span>Name: {c.name}</span>
-                  <span>Specialisation: {c.specialisation}</span>
-                </div>
-              )}
-            </div>
-          );
-        })}
-      </div>
+      {content.length !== 0 ? (
+        <SimpleBar className="max-h-40 border-b-4">
+          {content.map((c, index) => {
+            return (
+              <div
+                className={`footerItemRow ${
+                  index === content.length - 1 ? "border-b-2" : ""
+                }`}
+                key={`resource-${index}`}
+              >
+                {title === "Resources" ? (
+                  <div className="flex flex-col">
+                    <span>Type: {c.type}</span>
+                    <span>Quantity: {c.quantity}</span>
+                  </div>
+                ) : (
+                  <div className="flex flex-col">
+                    <span>Name: {c.name}</span>
+                    <span>Specialisation: {c.specialisation}</span>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </SimpleBar>
+      ) : (
+        "No content"
+      )}
     </div>
   );
 };
